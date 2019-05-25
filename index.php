78e088a7158fe57c8f6e6ebe20db76af4704d5ee
JPontaque <jofra8barcelona@hotmail.com>
2019-05-25 21:31:47 +0200

    Primer commit - Navbar

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <title>Home</title>
+</head>
+<header>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="#"><img src="images/logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="true" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse collapse show" id="navbarsExample04" style="">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Inicio <i class="fas fa-home"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tienda <i class="fas fa-store-alt"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Carrito <i class="fas fa-shopping-cart"></i> (0)</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contacto <i class="fas fa-map-marked-alt"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Panel de control <i class="fas fa-atlas"></i></a>
+                </li>
+            </ul>
+            <ul class="navbar-nav navbar-right">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Usuario <i class="fas fa-address-card"></i></a>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="#">Mi cuenta <i class="fas fa-user"></i></a>
+                        <a class="dropdown-item" href="#">Cerrar sesión <i class="fas fa-sign-out-alt"></i></a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
+<body>
+
+    <script src="jquery/jquery-3.3.1.min.js"></script>
+    <script src="popper/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="jquery/jquery-3.3.1.min.js"></script>
+</body>
+<footer>
+</footer>
