8caa20261ef4ad2caa52d369b980ddfea3e70500
JPontaque <jofra8barcelona@hotmail.com>
2019-05-26 11:03:41 +0200

    Añadido footer y estilos

--- includes/footer.php
+++ includes/footer.php
@@ -3,5 +3,9 @@
 <script src="bootstrap/js/bootstrap.min.js"></script>
 <script src="jquery/jquery-3.3.1.min.js"></script>
 </body>
-<footer>
+<footer class="footer mt-auto py-3">
+    <div class="container">
+        <span class="text-muted">&copy; 1997-2019, Play4Ever.com o afiliados. Todos los derechos reservados.</span>
+    </div>
 </footer>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style/style.css">
     <title>Home</title>
 </head>
 <header>
